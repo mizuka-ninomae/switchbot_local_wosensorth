b986f6a8f24a91c0d88db97212cadc63d37ff3e9
mizuka-ninomae <mizuka_ninomae@icloud.com>
2021-03-26 23:16:54 +0900

    update

--- test_driver.js
+++ test_driver.js
@@ -1,6 +1,6 @@
 const LocalWoSensorTH = require('switchbot_local_wosensorth');
 let  blu_mac          = "XX:XX:XX:XX:XX:XX";
-let  noble_ctl_path   = "/home/pi/switchbot_local_wosensorth/";
+let  noble_ctl_path   = "/home/pi/homebridge-sensor/node_modules/switchbot_local_wosensorth/";
 
 let wosendor = new LocalWoSensorTH (blu_mac, noble_ctl_path, function (error, value) {
     console.log (value);
