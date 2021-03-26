f47d6bd61fd72812177de100409dd68925e49e5e
mizuka-ninomae <mizuka_ninomae@icloud.com>
2021-03-26 22:54:05 +0900

    update

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require ('./switchbot_local_wosensorth');  
---
+++ test_driver.js
@@ -0,0 +1,8 @@
+const LocalWoSensorTH = require('switchbot_local_wosensorth');
+let  blu_mac          = "XX:XX:XX:XX:XX:XX";
+let  noble_ctl_path   = "/home/pi/switchbot_local_wosensorth/";
+
+let wosendor = new LocalWoSensorTH (blu_mac, noble_ctl_path, function (error, value) {
+    console.log (value);
+    console.log (error);
+});
